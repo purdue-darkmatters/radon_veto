2a5163e75edc37b3e76138f0d9607d96d278c4a2
Juehang Qin <qin106@purdue.edu>
2018-06-21 15:41:03 -0400

    linting of config.py

--- config.py
+++ config.py
@@ -1,36 +1,49 @@
+'''config file and convenience functions for radon_veto module'''
 import numpy as np
 
 gridsize = [2, 2, 1]
-limit_box = [(-50,50), (-50,50), (-100,0)]
-steps = (int(np.ceil((limit_box[0][1]-limit_box[0][0])/(gridsize[0]))),int(np.ceil((limit_box[1][1]-limit_box[1][0])/(gridsize[1]))),int(np.ceil((limit_box[2][1]-limit_box[2][0])/(gridsize[2]))))
+limit_box = [(-50, 50), (-50, 50), (-100, 0)]
+steps = (int(np.ceil((limit_box[0][1]-limit_box[0][0])/(gridsize[0]))),
+         int(np.ceil((limit_box[1][1]-limit_box[1][0])/(gridsize[1]))),
+         int(np.ceil((limit_box[2][1]-limit_box[2][0])/(gridsize[2]))))
 
 x_list = np.linspace(limit_box[0][0]+gridsize[0]/2, limit_box[0][1]-gridsize[0]/2, steps[0])
 y_list = np.linspace(limit_box[1][0]+gridsize[1]/2, limit_box[1][1]-gridsize[1]/2, steps[1])
 z_list = np.linspace(limit_box[2][0]+gridsize[2]/2, limit_box[2][1]-gridsize[2]/2, steps[2])
 
 
-def coord_from_index(index, gridsize, limit_box):
-    return (gridsize[0]*index[0]+gridsize[0]/2+limit_box[0][0],gridsize[1]*index[1]+gridsize[1]/2+limit_box[1][0],gridsize[2]*index[2]+gridsize[2]/2+limit_box[2][0])
+def coord_from_index(index, grids, lim_box):
+    '''Get coordinate values from indices'''
+    return (grids[0]*index[0]+grids[0]/2+lim_box[0][0],
+            grids[1]*index[1]+grids[1]/2+lim_box[1][0],
+            grids[2]*index[2]+grids[2]/2+lim_box[2][0])
 
 
-def index_from_coord(coord, gridsize, limit_box):
-    return (int(np.floor((coord[0]-limit_box[0][0])/gridsize[0])),int(np.floor((coord[1]-limit_box[1][0])/gridsize[1])),int(np.floor((coord[2]-limit_box[2][0])/gridsize[2])))
+def index_from_coord(coord, grids, lim_box):
+    '''Get indices from coordinate values'''
+    return (int(np.floor((coord[0]-lim_box[0][0])/grids[0])),
+            int(np.floor((coord[1]-lim_box[1][0])/grids[1])),
+            int(np.floor((coord[2]-lim_box[2][0])/grids[2])))
 
 
-def index_from_coord_float(coord, gridsize, limit_box):
-    return ((coord[0]-limit_box[0][0])/gridsize[0]-0.5,(coord[1]-limit_box[1][0])/gridsize[1]-0.5,(coord[2]-limit_box[2][0])/gridsize[2]-0.5)
+def index_from_coord_float(coord, grids, lim_box):
+    '''Get indices from coordinate values without rounding to integers'''
+    return ((coord[0]-lim_box[0][0])/grids[0]-0.5,
+            (coord[1]-lim_box[1][0])/grids[1]-0.5,
+            (coord[2]-lim_box[2][0])/grids[2]-0.5)
 
-threads=3
+
+threads = 2
 timestep = 5e7
 
-posrec_sigma = [0.3,0.3,0.15]
+posrec_sigma = [0.3, 0.3, 0.15]
 
 #There seem to be events below 96.9 and with radius outside 47.9. This needs investigating.
 
 #radius = 47.9
 radius = 48.5
 #height = 96.9
-height = 97.5 
+height = 97.5
 
 subd = 6 #subdivisions for interpolation and noise generation
 
@@ -42,4 +55,7 @@ noise_amplitude = 1e-11
 #noise_amplitude = 0
 
 def interp_index_from_coord(coord):
-    return (int(np.floor((coord[0]-limit_box[0][0])/(gridsize[0]/subd))), int(np.floor((coord[1]-limit_box[1][0])/(gridsize[0]/subd))), int(np.floor((coord[2]-limit_box[2][0])/(gridsize[0]/subd))))
+    '''Get interpolated index from coordinate values'''
+    return (int(np.floor((coord[0]-limit_box[0][0])/(gridsize[0]/subd))),
+            int(np.floor((coord[1]-limit_box[1][0])/(gridsize[0]/subd))),
+            int(np.floor((coord[2]-limit_box[2][0])/(gridsize[0]/subd))))
